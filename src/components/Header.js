ef6dae2c36c114297509fc99a5a74120a7d455f5
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-12-16 11:59:55 +0100

    login button and popup added

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,6 +13,7 @@ import {
 } from "@material-ui/core/styles";
 import { useHistory } from "react-router-dom";
 import { CryptoState } from "../CryptoContext";
+import AuthModal from "./Authentication/AuthModal";
 
 const useStyles = makeStyles((theme) => ({
   title: {
@@ -63,6 +64,7 @@ function Header() {
               <MenuItem value={"USD"}>USD</MenuItem>
               <MenuItem value={"EUR"}>EUR</MenuItem>
             </Select>
+            <AuthModal />
           </Toolbar>
         </Container>
       </AppBar>
