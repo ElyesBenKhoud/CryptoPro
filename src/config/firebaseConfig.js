e83a0fde4956a92581e36c6583adf8353ddb8224
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-12-16 11:41:31 +0100

    firebase installed

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDf7y0fQ9IOnfGn96uIq2mrttrS9GnxrXA",
+  authDomain: "crypto-hunter-75759.firebaseapp.com",
+  projectId: "crypto-hunter-75759",
+  storageBucket: "crypto-hunter-75759.appspot.com",
+  messagingSenderId: "38439192849",
+  appId: "1:38439192849:web:88f8716d8a92947754e4b0",
+};
+
+export default firebaseConfig;
